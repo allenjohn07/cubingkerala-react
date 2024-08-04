a48b9fc253a03d91056f9569b6ce1b49f3374412
Allen John <allenjohnmonapallil@gmail.com>
2024-08-04 08:09:31 +0530

    some fixes #6

--- frontend/src/pages/Competitions.jsx
+++ frontend/src/pages/Competitions.jsx
@@ -77,39 +77,44 @@ const Competitions = () => {
               <h1 className="text-3xl font-bold mb-8 text-center">Competitions</h1>
               <div className="mb-12">
                 <h2 className="text-2xl font-semibold mb-4">Upcoming Competitions</h2>
-                <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 lg:grid-cols-4">
-                  {
-                    upcomingCompetitions?.map((upComp, index) => (
-                      <Card
-                        key={index}
-                        onClick={() => navigate(`/competitions/${upComp.id}`)}
-                        className="max-w-full bg-zinc-900 rounded-md border-none hover:scale-105 transition-all cursor-pointer relative"
-                      >
-                        <div>
-                          <h5 className="text-lg font-bold tracking-tight text-gray-200 dark:text-white">
-                            {upComp?.name}
-                          </h5>
-                          <Badge className='absolute top-1 right-1' color="warning">Upcoming</Badge>
-                        </div>
-                        <div>
-                          <div className='flex flex-col items-start gap-1'>
-                            <span className='text-sm flex items-center gap-1 text-success'>{new Date(upComp?.date?.from).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })} - {new Date(upComp?.date?.till).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</span>
-                            <span className='text-sm'>{upComp?.venue?.address}</span>
-                            <div className='text-xs'>
-                              {
-                                upComp?.events.map((event, index) => (
-                                  <Tooltip content={event} key={index}>
-                                    <span className={`cubing-icon event-${event} pr-2`}></span>
-                                  </Tooltip>
-                                ))
-                              }
+                {
+                  upcomingCompetitions.length > 0 ? <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 lg:grid-cols-4">
+                    {
+                      upcomingCompetitions?.map((upComp, index) => (
+                        <Card
+                          key={index}
+                          onClick={() => navigate(`/competitions/${upComp.id}`)}
+                          className="max-w-full bg-zinc-900 rounded-md border-none hover:scale-105 transition-all cursor-pointer relative"
+                        >
+                          <div>
+                            <h5 className="text-lg font-bold tracking-tight text-gray-200 dark:text-white">
+                              {upComp?.name}
+                            </h5>
+                            <Badge className='absolute top-1 right-1' color="warning">Upcoming</Badge>
+                          </div>
+                          <div>
+                            <div className='flex flex-col items-start gap-1'>
+                              <span className='text-sm flex items-center gap-1 text-success'>{new Date(upComp?.date?.from).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })} - {new Date(upComp?.date?.till).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</span>
+                              <span className='text-sm'>{upComp?.venue?.address}</span>
+                              <div className='text-xs'>
+                                {
+                                  upComp?.events.map((event, index) => (
+                                    <Tooltip content={event} key={index}>
+                                      <span className={`cubing-icon event-${event} pr-2`}></span>
+                                    </Tooltip>
+                                  ))
+                                }
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      </Card>
-                    ))
-                  }
-                </div>
+                        </Card>
+                      ))
+                    }
+                  </div> : <div className='py-8 lg:py-20 text-start'>
+                    <h1 className="text-lg lg:text-3xl font-semibold text-success">
+                    New competitions are on the way!</h1>
+                  </div>
+                }
                 {
                   upcomingCompetitions.length <= compsPerPage ? null : (
                     <div className="flex overflow-x-auto justify-center py-5">
